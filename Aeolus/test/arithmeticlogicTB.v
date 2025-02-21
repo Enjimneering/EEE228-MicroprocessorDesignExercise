3afd830de04b23c2a9f6df808d22f96ee211a09d
James <162901067+Enjimneering@users.noreply.github.com>
2025-02-21 11:26:04 +0000

    fixed alutb

--- Aeolus/test/arithmeticlogicTB.v
+++ Aeolus/test/arithmeticlogicTB.v
@@ -3,7 +3,7 @@
 `timescale 1ns/1ns
 `include "src/ALU.v"
 
-`define assert(signal, value) if (signal !== value) begin  $display("ASSERTION FAILED for OPCODE: %4b at time %0t ns : %m signal  !=  value expected: %5b, got: %5b", OPCODE , $time , value, signal); $finish; end
+`define assert(signal, value) if (signal !== value) begin  $display("ASSERTION FAILED for CONTROL SIGNAL: %4b at time %0t ns : %m signal  !=  value expected: %5b, got: %5b", CONTROL , $time , value, signal); $finish; end
 
 module ALUTb();
 
@@ -15,21 +15,21 @@ module ALUTb();
     
     integer i;
 
-    reg [7:0] OPCODE;
+    reg [8:0] CONTROL;
 
     ArithmeticLogicUnit alu( 
 
     .clk(CLK),
     .reset(RESET),
-    .ADD(OPCODE[0]),
-    .SUB(OPCODE[1]),
-    .LSR(OPCODE[2]),
-    .LSH(OPCODE[3]),
-    .RSH(OPCODE[4]),
-    .AND(OPCODE[5]),
-    .OR (OPCODE[6]),
-    .XOR(OPCODE[7]),
-    .INV(OPCODE[8]),
+    .ADD(CONTROL[0]),
+    .SUB(CONTROL[1]),
+    .LSR(CONTROL[2]),
+    .LSH(CONTROL[3]),
+    .RSH(CONTROL[4]),
+    .AND(CONTROL[5]),
+    .OR (CONTROL[6]),
+    .XOR(CONTROL[7]),
+    .INV(CONTROL[8]),
     .in1(IN1),
     .in2(IN2), 
     .out(ALU_OUT),
@@ -41,55 +41,55 @@ module ALUTb();
         $dumpfile("test/vcd/arithmeticlogicdump.vcd");
         $dumpvars(0, ALUTb);
         
-        CLK = 1; IN1 = 4'b0111; IN2 = 4'b0101; OPCODE = 0;
+        CLK = 1; IN1 = 4'b0111; IN2 = 4'b0101; CONTROL = 0;
 
         #15 $display("ALU TEST");
-        OPCODE = 1;
+        CONTROL = 1;
         $display("Testing ADD Instruction..."); 
         #10 `assert (ALU_OUT, (IN1 + IN2))
-        //$display("ADD Instruction impliemented successfully.");
+        $display("ADD Instruction impliemented successfully.");
 
-        #5 OPCODE = OPCODE << 1;    // sub
+        #5 CONTROL = CONTROL << 1;    // sub
         $display("Testing SUB Instruction...");
         #15 `assert (ALU_OUT, (IN1 - IN2))
-        //$display("SUB Instruction impliemented successfully.");
+        $display("SUB Instruction impliemented successfully.");
 
-        #5 OPCODE = OPCODE << 1;   // lsr
+        #5 CONTROL = CONTROL << 1;   // lsr
         $display("Testing LSR Instruction...");
         #15 `assert (alu.sr.dataReg, IN1);
-        //$display("LSH Instruction impliemented successfully.");
+        $display("LSH Instruction impliemented successfully.");
 
-        #5 OPCODE = OPCODE << 1;   // lsh
+        #5 CONTROL = CONTROL << 1;   // lsh
         $display("Testing LSH Instruction...");
         #15 `assert (ALU_OUT, (IN1 << 1) );
-        //$display("LSH Instruction impliemented successfully.");
+        $display("LSH Instruction impliemented successfully.");
 
-        #5 OPCODE = OPCODE << 1;   // rsh
+        #5 CONTROL = CONTROL << 1;   // rsh
         $display("Testing RSH Instruction...");
         #15 `assert (ALU_OUT, (IN1 >> 1) );
-        //$display("RSH Instruction impliemented successfully.");
+        $display("RSH Instruction impliemented successfully.");
 
-        #5 OPCODE = OPCODE << 1;   // and
+        #5 CONTROL = CONTROL << 1;   // and
         $display("Testing AND Instruction...");
         #15 `assert (ALU_OUT, (IN1[3:0] & IN2[3:0]));
-        //$display("AND Instruction impliemented successfully.");
+        $display("AND Instruction impliemented successfully.");
 
-        #5 OPCODE = OPCODE << 1;   // or
+        #5 CONTROL = CONTROL << 1;   // or
         $display("Testing OR Instruction...");
         #15 `assert (ALU_OUT, (IN1[3:0] | IN2[3:0]) );
-        //$display("OR Instruction impliemented successfully.");
+        $display("OR Instruction impliemented successfully.");
 
-        #5 OPCODE = OPCODE << 1;   // xor
+        #5 CONTROL = CONTROL << 1;   // xor
         $display("Testing XOR Instruction...");
         #15 `assert (ALU_OUT, (IN1[3:0] ^ IN2[3:0]));
-        //$display("XOR Instruction impliemented successfully.");
+        $display("XOR Instruction impliemented successfully.");
 
-        #5 OPCODE = OPCODE << 1;   // xor
+        #5 CONTROL = CONTROL << 1;   // inv
         $display("Testing INV Instruction...");
         #15 `assert (ALU_OUT, (~IN1[3:0]));
-        //$display("NOT Instruction impliemented successfully.");
+        $display("INV Instruction impliemented successfully.");
 
-        $display("ALL TEST SUCCESSFUL!"); $finish; 
+        $display("ALL TESTS SUCCESSFUL!"); $finish; 
     end
 
     always  begin
