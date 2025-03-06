e38729f8bbf42015953d8e12f1d02bf40a11510a
James <162901067+Enjimneering@users.noreply.github.com>
2025-03-06 13:16:48 +0000

    Renamed Top Module

--- Aeolus/test/arithmeticlogicTB.v
+++ Aeolus/test/arithmeticlogicTB.v
@@ -1,47 +1,56 @@
-// Logic Testbench
+// Arithmetic Logic Unit Testbench
 
 `timescale 1ns/1ns
-`include "src/ALU.v"
+`include "src/ALU.v"     // contains ALU module
+`include "test/macros.v" // defines assert macro
 
-`define assert(signal, value) if (signal !== value) begin  $display("ASSERTION FAILED for CONTROL SIGNAL: %4b at time %0t ns : %m signal  !=  value expected: %5b, got: %5b", CONTROL , $time , value, signal); $finish; end
+/*
+    Description:
+        This testbench tests the combinational outputs of the ALU by changing
+        the control signals and inputs going into the module, and making asseertions
+        on the alu output wire. 
+    Objectives:
+        Test Addition and Subtraction (with overflow/underflow)
+        Test AND, OR, XOR and INV functionality
+    Success Criteria:
+        The test results are visible in the console, if the prgram terminates with
+        an assertion error, then  at least one of the logical functions has been implimented 
+        incorrectly.
+*/
 
 module ALUTb();
 
     reg        CLK;
-    reg        RESET;
+    reg        RESET;   // todo: impliment/test reset functionality?
     reg  [3:0] IN1, IN2;
     wire [3:0] ALU_OUT;
     wire       OVERFLOW;
     
     integer i;
 
-    reg [8:0] CONTROL;
+    reg [6:0] CONTROL;
 
-    ArithmeticLogicUnit alu( 
-
-    .clk(CLK),
-    .reset(RESET),
-    .ADD(CONTROL[0]),
-    .SUB(CONTROL[1]),
-    .LSR(CONTROL[2]),
-    .LSH(CONTROL[3]),
-    .RSH(CONTROL[4]),
-    .AND(CONTROL[5]),
-    .OR (CONTROL[6]),
-    .XOR(CONTROL[7]),
-    .INV(CONTROL[8]),
-    .in1(IN1),
-    .in2(IN2), 
-    .out(ALU_OUT),
-    .overflow(OVERFLOW)
-
+    ArithmeticLogicUnit alu ( 
+        .clk(CLK),
+        .reset(RESET),
+        .ADD(CONTROL[0]),
+        .SUB(CONTROL[1]),
+        .AND(CONTROL[2]),
+        .OR (CONTROL[3]),
+        .XOR(CONTROL[4]),
+        .INV(CONTROL[5]),
+        .in1(IN1),  
+        .in2(IN2), 
+        .out(ALU_OUT),
+        .overflow(OVERFLOW)
     );
+
     initial begin
 
         $dumpfile("test/vcd/arithmeticlogicdump.vcd");
         $dumpvars(0, ALUTb);
         
-        CLK = 1; IN1 = 4'b0111; IN2 = 4'b0101; CONTROL = 0;
+        CLK = 1; IN1 = 4'b0111; IN2 = 4'b0101; CONTROL = 0; 
 
         #15 $display("ALU TEST");
         CONTROL = 1;
@@ -53,21 +62,6 @@ module ALUTb();
         $display("Testing SUB Instruction...");
         #15 `assert (ALU_OUT, (IN1 - IN2))
         $display("SUB Instruction impliemented successfully.");
-
-        #5 CONTROL = CONTROL << 1;   // lsr
-        $display("Testing LSR Instruction...");
-        #15 `assert (alu.sr.dataReg, IN1);
-        $display("LSH Instruction impliemented successfully.");
-
-        #5 CONTROL = CONTROL << 1;   // lsh
-        $display("Testing LSH Instruction...");
-        #15 `assert (ALU_OUT, (IN1 << 1) );
-        $display("LSH Instruction impliemented successfully.");
-
-        #5 CONTROL = CONTROL << 1;   // rsh
-        $display("Testing RSH Instruction...");
-        #15 `assert (ALU_OUT, (IN1 >> 1) );
-        $display("RSH Instruction impliemented successfully.");
 
         #5 CONTROL = CONTROL << 1;   // and
         $display("Testing AND Instruction...");
--- Aeolus/test/cpuTB.v
+++ Aeolus/test/cpuTB.v
@@ -1,9 +1,22 @@
-// Logic Testbench
+// CPU Program Execution Testbench
 
-`timescale 1ns/1ns
-`include "src/Top.v"
+`timescale 1ns/1ps
+`include "src/AeolusCPU.v"  
+`include "test/macros.v"    // defines assertion macro
 
-`define assert(signal, value) if (signal !== value) begin  $display("ASSERTION FAILED for OPCODE: %4b at time %0t ns : %m signal  !=  value expected: %5b, got: %5b", OPCODE , $time , value, signal); $finish; end
+/*
+    Description:
+        This testbench tests the combinational outputs of the ALU by changing
+        the control signals and inputs going into the module, and making asseertions
+        on the alu output wire. 
+    Objectives:
+        Test Addition and Subtraction (with overflow/underflow)
+        Test AND, OR, XOR and INV functionality
+    Success Criteria:
+        The test results are visible in the console, if the prgram terminates with
+        an assertion error, then  at least one of the logical functions has been implimented 
+        incorrectly.
+*/
 
 module CPUTb();
 
@@ -21,12 +34,12 @@ module CPUTb();
         .cpuOut(CPU_OUT)
     );
 
-    initial begin
+    initial begin 
 
         $dumpfile("test/vcd/CPUadddump.vcd");
         $dumpvars(0, CPUTb);
         
-        CLK = 1; RESET = 1; SWITCHES[7:0] = 8'b1010_1010; 
+        CLK = 1; RESET = 1; SWITCHES[7:0] = 8'b0010_0011; 
         #15 RESET = 0;
         #3300 $display("program complete!"); $finish; 
     end
